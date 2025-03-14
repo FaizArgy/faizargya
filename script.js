84256e8e32c05ca42f3a4baa0b1f6186b2855232
FaizArgy <faizargyanto@gmail.com>
2025-03-15 04:30:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// navbar
+
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
+})
+
